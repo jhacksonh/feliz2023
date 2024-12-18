61f61222171e65110a89766b1fce768287392ae4
Jacson Santos <dev.jhacks@gmail.com>
2024-12-17 22:54:36 -0300

    ajuste nos códigos js

--- js/js.js
+++ js/js.js
@@ -5,19 +5,16 @@ let atc_content = document.querySelector(".atc_content");
 let form_secion = document.querySelector(".section_form");
 let content_secion = document.querySelector(".section_content");
 btn_cfmr.addEventListener('click', () => {
-    /*e.preventDefault();*/
     let n = name.value.toLowerCase();
     let p = pass.value.toLowerCase();
     if (n !== "" && p !== "") {
-        //name.value = name.value.replace(/\.0\./g,'\"');
-        //name.value = Encriptar(n);
-        //name.value = Descriptar(n);
         let nEncript = Encriptar(n);
         let pEncript = Encriptar(p);
-        if (Mensagem(nEncript, pEncript) != 0) {
+        let mensagem = Mensagem(nEncript, pEncript);
+        if (mensagem != 0) {
             form_secion.style.display = "none";
             content_secion.style.display = "block";
-            atc_content.innerHTML = `${Mensagem(nEncript, pEncript)}`;
+            atc_content.innerHTML = `${mensagem}`;
         } else {
             alert("dados não conferem");
         }
