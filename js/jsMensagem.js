34a2070baae2311c213a1352a83e9994fcff38bc
Jacson Santos <dev.jhacks@gmail.com>
2024-12-24 14:51:29 -0300

    ...

--- js/jsMensagem.js
+++ js/jsMensagem.js
@@ -1,7 +1,6 @@
 function Mensagem(nome, senha) {
     let i;
     for (i in DADOS) {
-        console.log(Descriptar(DADOS[i][0],"‌")+" "+Descriptar(DADOS[i][1],"‌"));
         if (nome == DADOS[i][0] && senha == DADOS[i][1]){
             return Descriptar(DADOS[i][2],"‌");
         }
